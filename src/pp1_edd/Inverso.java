742cba225feb7ee69ba5641a1b036d11a8cc7e5c
Brandon Lanzoni <brandongabriel120105@gmail.com>
2025-09-29 16:49:43 -0600

    Agregar Inverso.java y README respetando .gitignore

--- src/pp1_edd/Inverso.java
+++ src/pp1_edd/Inverso.java
@@ -34,5 +34,20 @@ public class Inverso {
     }
 }
     
-    
+ /*  
+//Brandon Lanzoni carnet: FI25049122
 
+//utilice tik tok y youtube como guias para el proceso y la ia la use solamente para verificar que si funcionara todo y subir el archivo a github
+
+¿Es posible implementar una solución equivalente pero de comportamiento iterativo?
+si
+
+¿Cree que hay alguna otra manera recursiva de generar el mismo resultado?
+Muy rpobablemnte si
+
+
+¿Qué relación observa entre el algoritmo para invertir un número natural (calculando y utilizando los cocientes y residuos de las divisiones) con las estrategias para pasar de una base numérica a otra?
+sigue la misma base logica de codigo
+
+
+*/
