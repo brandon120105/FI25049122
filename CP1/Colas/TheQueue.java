149b583c665b03046cf2c630fd06602585a19505
Brandon Lanzoni <brandongabriel120105@gmail.com>
2025-10-26 20:55:27 -0600

    Subiendo proyecto CP1 completo

---
+++ CP1/Colas/TheQueue.java
@@ -0,0 +1,79 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Random;
+
+public class TheQueue<Type> implements TheQueueInterface<Type> {
+
+    private Deque<Type> _queue;
+
+    public TheQueue() {
+        _queue = new ArrayDeque<Type>();
+    }
+
+    public void enqueue(Type item) {
+        _queue.add(item);
+    }
+
+    public Type dequeue() {
+        return _queue.isEmpty() ? null : _queue.remove();
+    }
+
+    public Type getFront() {
+        return _queue.peek();
+    }
+
+    public boolean isEmpty() {
+        return _queue.isEmpty();
+    }
+
+    public int getSize() {
+        return _queue.size();
+    }
+
+    public String[] getCodons() {
+        var size = _queue.size();
+        var count = size / 3;
+        String[] codons = new String[count];
+        for (int i = 0; i < count; i++) {
+            String codon = "";
+            for (int j = 0; j < 3; j++) {
+                codon += _queue.remove();
+            }
+            codons[i] = codon;
+        }
+        return codons;
+    }
+
+    public String print() {
+        return _queue.toString();
+    }
+
+    public static void main(String[] args) {
+        var amount = Integer.parseInt(args[0]);
+        String[] nucleotides = {
+                "A", // [0]
+                "C", // [1]
+                "G", // [2]
+                "T", // [3]
+        };
+        TheQueueInterface<String> queue = new TheQueue<String>();
+        var random = new Random();
+        for (; amount > 0; amount--) {
+            var index = random.nextInt(4);
+            var amino = nucleotides[index];
+            queue.enqueue(amino);
+        }
+        System.out.println("Filled {full}");
+        System.out.println(" ↳ print() → " + queue.print());
+        System.out.println("   ↳ getFront() → " + queue.getFront());
+        System.out.println("   ↳ getSize() → " + queue.getSize());
+        System.out.println("   ↳ isEmpty() → " + queue.isEmpty());
+        System.out.println("\ngetCodons() → " + Arrays.toString(queue.getCodons()));
+        System.out.println("\nEnd {empty}");
+        System.out.println(" ↳ print() → " + queue.print());
+        System.out.println("   ↳ getFront() → " + queue.getFront());
+        System.out.println("   ↳ getSize() → " + queue.getSize());
+        System.out.println("   ↳ isEmpty() → " + queue.isEmpty());
+    }
+}
